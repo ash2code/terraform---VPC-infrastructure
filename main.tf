555c47b905b0a5a0b082f35408df3ef9b708622b
ash2code <ashoksoma95@gmail.com>
2023-12-13 11:05:26 +0000

    initial commit - terraform output variables

---
+++ main.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAXPL7YFM2XVEXZ4BR"
+  secret_key = "Jik0oNM90f9wIG+qYZ68iL+meFwurq787mrd+0e5"
+}
---
+++ output.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0a7cf821b91bcccbc"
+  instance_type = "t2.micro"
+  tags = {
+   name = "example-ec2"
+} 
+}
